6b1f0fb2e46fbfa0027042cc5b8e504638ffc211
polygonnedpotato <73083772+polygonnedpotato@users.noreply.github.com>
2020-10-19 11:14:10 -0700

    added bat to restart explorer

---
+++ Restart Explorer.bat
@@ -0,0 +1,8 @@
+taskkill /f /im explorer.exe
+taskkill /f /im explorer.exe
+taskkill /f /im explorer.exe
+taskkill /f /im explorer.exe
+taskkill /f /im explorer.exe
+taskkill /f /im dwm.exe
+start dwm.exe
+start explorer.exe
